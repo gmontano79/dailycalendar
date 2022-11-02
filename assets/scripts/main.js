15321b6fadc8b3509b0f5a12dd958c545f8ec1bc
gmontano79 <gmontano79@gmail.com>
2022-11-02 17:15:57 -0400

    completed code for color changing depending on time

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,4 @@
 //  **** TODAY DATE FORMAT ****
-
 function date() {
   var today = moment();
   $("#currentDay").text(today.format("MMM Do, YYYY"));
@@ -7,169 +6,47 @@ function date() {
 date()
 
 // ***************************************
-// **********  TIME BLOCK 9AM ************
-
-//  SAVE EVENT TO LOCAL STORAGE
+// **********  EVENTS ************
 
 
-var saveBtn9 = document.querySelector(".saveBtn9");
-saveBtn9.addEventListener("click", saveLocalStorage9);
-function saveLocalStorage9() {
-  var userInput9 = document.getElementById("userinput9").value;
-  localStorage.setItem("ls_userInput9", userInput9);
+var arrTimes = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
+
+function testFunction(e) {
+  var value = document.querySelector("#userinput" + e.target.dataset.time).value;
+  localStorage.setItem("ls_userInput" + e.target.dataset.time, value);
+  var confirmation = document.querySelector(".confirmation");
+  confirmation.style.display = "block";
+
+
+  // timeout for confirmation
+
+  setTimeout(function () {
+    confirmation.style.display = "none";
+  }, 3000)
 }
 
-//  INPUT SAVED EVENT ON THE PLANNER EVENT
-var savedInput9 = localStorage.getItem("ls_userInput9");
-var userInput9 = document.getElementById("userinput9");
-userInput9.textContent = savedInput9;
-userInput9.setAttribute("value", savedInput9);
 
 
-// ***************************************
-// **********  TIME BLOCK 10AM ************
+//  save input into local stoage and text area
 
-//  SAVE EVENT TO LOCAL STORAGE
+for (i = 0; i < arrTimes.length; i++) {
+  var currentBtn = document.querySelector(".saveBtn" + arrTimes[i])
+  currentBtn.addEventListener("click", testFunction)
 
-var saveBtn10 = document.querySelector(".saveBtn10");
-saveBtn10.addEventListener("click", saveLocalStorage10);
-function saveLocalStorage10() {
-  var userInput10 = document.getElementById("userinput10").value;
-  localStorage.setItem("ls_userInput10", userInput10);
+  var currentTextArea = document.querySelector("#userinput" + arrTimes[i])
+  currentTextArea.value = localStorage.getItem("ls_userInput" + arrTimes[i])
+
+
+
+  // color past, present and future colors
+
+  var currentTime = parseInt(moment().format("HH"));
+
+  if (currentTime > i + 9) {
+    currentTextArea.setAttribute("class", "events col-9 past")
+  } else if (currentTime == i + 9) {
+    currentTextArea.setAttribute("class", "events col-9 present")
+  } else {
+    currentTextArea.setAttribute("class", "events col-9 future")
+  }
 }
-
-//  INPUT SAVED EVENT ON THE PLANNER EVENT
-var savedInput10 = localStorage.getItem("ls_userInput10");
-console.log(savedInput10);
-
-var userInput10 = document.getElementById("userinput10");
-userInput10.textContent = savedInput10;
-userInput10.setAttribute("value", savedInput10);
-
-
-// ***************************************
-// **********  TIME BLOCK 11AM ************
-
-//  SAVE EVENT TO LOCAL STORAGE
-
-var saveBtn11 = document.querySelector(".saveBtn11");
-saveBtn11.addEventListener("click", saveLocalStorage11);
-function saveLocalStorage11() {
-  var userInput11 = document.getElementById("userinput11").value;
-  localStorage.setItem("ls_userInput11", userInput11);
-}
-
-//  INPUT SAVED EVENT ON THE PLANNER EVENT
-var savedInput11 = localStorage.getItem("ls_userInput11");
-var userInput11 = document.getElementById("userinput11");
-userInput11.textContent = savedInput11;
-userInput11.setAttribute("value", savedInput11);
-
-// ***************************************
-// **********  TIME BLOCK 12PM ************
-
-//  SAVE EVENT TO LOCAL STORAGE
-
-var saveBtn12 = document.querySelector(".saveBtn12");
-saveBtn12.addEventListener("click", saveLocalStorage12);
-function saveLocalStorage12() {
-  var userInput12 = document.getElementById("userinput12").value;
-  localStorage.setItem("ls_userInput12", userInput12);
-}
-
-//  INPUT SAVED EVENT ON THE PLANNER EVENT
-var savedInput12 = localStorage.getItem("ls_userInput12");
-var userInput12 = document.getElementById("userinput12");
-userInput12.textContent = savedInput12;
-userInput12.setAttribute("value", savedInput12);
-
-// ***************************************
-// **********  TIME BLOCK 1PM ************
-
-//  SAVE EVENT TO LOCAL STORAGE
-
-var saveBtn1 = document.querySelector(".saveBtn1");
-saveBtn1.addEventListener("click", saveLocalStorage1);
-function saveLocalStorage1() {
-  var userInput1 = document.getElementById("userinput1").value;
-  localStorage.setItem("ls_userInput1", userInput1);
-}
-
-//  INPUT SAVED EVENT ON THE PLANNER EVENT
-var savedInput1 = localStorage.getItem("ls_userInput1");
-var userInput1 = document.getElementById("userinput1");
-userInput1.textContent = savedInput1;
-userInput1.setAttribute("value", savedInput1);
-
-
-// ***************************************
-// **********  TIME BLOCK 2PM ************
-
-//  SAVE EVENT TO LOCAL STORAGE
-
-var saveBtn2 = document.querySelector(".saveBtn2");
-saveBtn2.addEventListener("click", saveLocalStorage2);
-function saveLocalStorage2() {
-  var userInput2 = document.getElementById("userinput2").value;
-  localStorage.setItem("ls_userInput2", userInput2);
-}
-
-//  INPUT SAVED EVENT ON THE PLANNER EVENT
-var savedInput2 = localStorage.getItem("ls_userInput2");
-var userInput2 = document.getElementById("userinput2");
-userInput2.textContent = savedInput2;
-userInput2.setAttribute("value", savedInput2);
-
-// ***************************************
-// **********  TIME BLOCK 3PM ************
-
-//  SAVE EVENT TO LOCAL STORAGE
-
-var saveBtn3 = document.querySelector(".saveBtn3");
-saveBtn3.addEventListener("click", saveLocalStorage3);
-function saveLocalStorage3() {
-  var userInput3 = document.getElementById("userinput3").value;
-  localStorage.setItem("ls_userInput3", userInput3);
-}
-
-//  INPUT SAVED EVENT ON THE PLANNER EVENT
-var savedInput3 = localStorage.getItem("ls_userInput3");
-var userInput3 = document.getElementById("userinput3");
-userInput3.textContent = savedInput3;
-userInput3.setAttribute("value", savedInput3);
-
-// ***************************************
-// **********  TIME BLOCK 4PM ************
-
-//  SAVE EVENT TO LOCAL STORAGE
-
-var saveBtn4 = document.querySelector(".saveBtn4");
-saveBtn4.addEventListener("click", saveLocalStorage4);
-function saveLocalStorage4() {
-  var userInput4 = document.getElementById("userinput4").value;
-  localStorage.setItem("ls_userInput4", userInput4);
-}
-
-//  INPUT SAVED EVENT ON THE PLANNER EVENT
-var savedInput4 = localStorage.getItem("ls_userInput4");
-var userInput4 = document.getElementById("userinput4");
-userInput4.textContent = savedInput4;
-userInput4.setAttribute("value", savedInput4);
-
-// ***************************************
-// **********  TIME BLOCK 5PM ************
-
-//  SAVE EVENT TO LOCAL STORAGE
-
-var saveBtn5 = document.querySelector(".saveBtn5");
-saveBtn5.addEventListener("click", saveLocalStorage5);
-function saveLocalStorage5() {
-  var userInput5 = document.getElementById("userinput5").value;
-  localStorage.setItem("ls_userInput5", userInput5);
-}
-
-//  INPUT SAVED EVENT ON THE PLANNER EVENT
-var savedInput5 = localStorage.getItem("ls_userInput5");
-var userInput5 = document.getElementById("userinput5");
-userInput5.textContent = savedInput5;
-userInput5.setAttribute("value", savedInput5);
